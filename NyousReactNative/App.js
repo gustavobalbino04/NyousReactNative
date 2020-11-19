b905d008d3dc0e44120e7bf837e0686ad80e530c
gustavobalbino04 <gubalbino15@gmail.com>
2020-11-19 00:37:47 -0300

    Nyous iniciada

---
+++ NyousReactNative/App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Login 
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
